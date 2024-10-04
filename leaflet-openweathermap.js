a3dca13b82593774a95e1a827b0a86828348fda0
Mohd Sufian Othman <myphpmaster@gmail.com>
2024-10-04 09:49:30 +0800

    Update leaflet-openweathermap.js

--- leaflet-openweathermap.js
+++ leaflet-openweathermap.js
@@ -6,7 +6,7 @@
 
 L.OWM = L.TileLayer.extend({
 	options: {
-		appId: 'GET_YOUR_OWN_APPID', /* pass your own AppId as parameter when creating the layer. Get your own AppId at https://www.openweathermap.org/appid */
+		appId: '040d9844cea8ea74a534b7910954fcd1', /* pass your own AppId as parameter when creating the layer. Get your own AppId at https://www.openweathermap.org/appid */
 		baseUrl: "https://{s}.tile.openweathermap.org/map/{layername}/{z}/{x}/{y}.png",
 		maxZoom: 18,
 		showLegend: true,
