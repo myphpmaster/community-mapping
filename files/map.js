cd9eb8aee3aa2a17e94a17d2a630c582adc98d74
Mohd Sufian Othman <myphpmaster@gmail.com>
2024-10-05 15:12:18 +0800

    Update map.js

--- files/map.js
+++ files/map.js
@@ -342,15 +342,9 @@ function initMap() {
 		Geomet: L.tileLayer.wms('https://geo.weather.gc.ca/geomet?', {
 			layers: 'GDPS.ETA_TT',
 			version: '1.3.0',
-			opacity: 0.5,
-		}),
+			opacity: 0.5
+		})
 
-		Arcgis: L.esri.featureLayer({
-			url: "https://services3.arcgis.com/G1WwgjpwOa4JbyeG/arcgis/rest/services/Datensatz/FeatureServer",
-			style: function () {
-			  return { color: "#70ca49", weight: 2 };
-			}
-		  })
 	};
 	
 	L.control.layers(weather).addTo(map);
