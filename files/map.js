e25c5802be052c40d30456ed564e14ba90eb5acc
Mohd Sufian Othman <myphpmaster@gmail.com>
2024-10-06 17:36:32 +0800

    update geojson

--- files/map.js
+++ files/map.js
@@ -424,12 +424,19 @@ async function loadErosion() {
 			if(feature.properties.name){
 				content = '<h3>' + feature.properties.name + '</h3>';
 			}
-			if(feature.properties.text){
+			if(feature.properties.title){
+				content += '<strong>' + feature.properties.title + '</strong><br/>';
+			} else {
 				content += '<strong>Coastal Erosion</strong><br/>';
+			}
+			if(feature.properties.text){
 				content += '<p>' + feature.properties.text + '</p>';
 			}
 			if(feature.properties.link){
 				content += '<p><a href="' + feature.properties.link + '" target="_blank">Read more</p>';
+			}
+			if(feature.properties.img){
+				content += '<p><img src="' + feature.properties.img + '" width="100%" height="auto;"/></p>';
 			}
 			layer.bindPopup(content);		
 		},
